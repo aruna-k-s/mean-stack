c656f3a5a2aaba99347755fa60d06eded6430217
aruna ks <call2arunks@gmail.com>
2020-03-17 19:42:36 +0530

    comit

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,43 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req,res,next) =>{
-    console.log("hi i am app from express");
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET , POST, PATCH, PUT, DELETE, OPTIONS");
     next();
 })
 
-app.use((req,res,next)=>{
-    res.send("i am response");
+app.post('/api/post',(req,res,next) => {
+    const data = req.body;
+    console.log(data);
+    res.status(200).json({
+        message:'response recorded successfully'
+    })
+})
+
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: '1234',
+            content: 'hi i am data1',
+            title: 'its titel 1'
+        },
+
+        {
+            id: '1234',
+            content: 'hi i am data1',
+            title: 'its titel 1'
+        }
+    ]
+    res.status(200).json({
+        message: 'data as been successfully viewed',
+        posts: posts
+    })
 })
 
 module.exports = app;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  posts = [];
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.callApi();
+    this.postApi();
+  }
+
+  callApi() {
+    this.http.get('http://localhost:3000/api/posts').subscribe(res => {
+      this.posts = res['posts'];
+    });
+  }
+  postApi() {
+    this.http.post("http://localhost:3000/api/post", {
+      title: 'hi its title',
+      content: 'hi its content 1'
+    }).subscribe(res => {
+      console.log(res);
+    })
+  }
 }
